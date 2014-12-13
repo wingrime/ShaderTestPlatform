f0331290c21061d8262f6c4593f4f591ec69f491
Alexsey Shestacov <wingrime@gmail.com>
2014-12-13 20:40:22 +0500

    Add mtl parser routines to MTLParser class

--- Sources/obj_model.h
+++ Sources/obj_model.h
@@ -10,9 +10,7 @@
 #include "mat_math.h"
 #include "viewport.h"
 #include "r_texture.h"
-#if OLD_SERIALIZE
-#include "c_config.h"
-#endif
+
 #include "r_projmat.h"
 
 #include "r_context.h"
@@ -480,93 +478,31 @@ CObjMeshParser::CObjMeshParser(const std::string& fname)
     IsReady = true;
 }
 
+class MTLParser {
+public:
+     MTLParser(const std::string& fname);
+     std::unordered_map<std::string, std::shared_ptr<CMTLMaterial> > d_materials;
 
-std::string ParseNEWMTL(const std::string& str) {
-    char buf[256];
-    sscanf(str.c_str(),"newmtl %80s",buf);
-    return std::string(buf);
-}
+private:
 
-float ParseNs(const std::string& v_desc) {
-    float r = 0.0;
-    sscanf(v_desc.c_str()," Ns %f", &r);
-    return r;
-}
-float ParseNi(const std::string& v_desc) {
-    float r = 0.0;
-    sscanf(v_desc.c_str()," Ni %f", &r);
-    return r;
-}
-float Parsed(const std::string& v_desc) {
-    float r = 0.0;
-    sscanf(v_desc.c_str()," d %f", &r);
-    return r;
-}
-
-int Parseillum(const std::string& v_desc) {
-    int r = 0;
-    sscanf(v_desc.c_str()," illum %d", &r);
-    return r;
-}
-
-CMTLColor ParseKa(const std::string& v_desc) {
-    CMTLColor c;
-    sscanf(v_desc.c_str()," Ka %f %f %f", &c.r, &c.g, &c.b);
-    return c;
-}
-CMTLColor ParseKd(const std::string& v_desc) {
-    CMTLColor c;
-    sscanf(v_desc.c_str()," Kd %f %f %f", &c.r, &c.g, &c.b);
-    return c;
-}
-CMTLColor ParseKs(const std::string& v_desc) {
-    CMTLColor c;
-    sscanf(v_desc.c_str()," Ks %f %f %f", &c.r, &c.g, &c.b);
-    return c;
-}
-CMTLColor ParseKe(const std::string& v_desc) {
-    CMTLColor c;
-    sscanf(v_desc.c_str()," Ke %f %f %f", &c.r, &c.g, &c.b);
-    return c;
-}
-CMTLColor ParseTf(const std::string& v_desc) {
-    CMTLColor c;
-    sscanf(v_desc.c_str()," Tf %f %f %f", &c.r, &c.g, &c.b);
-    return c;
-}
-
-std::string Parsemap_Ka(const std::string& str) {
-    char buf[256];
-    sscanf(str.c_str()," map_Ka %80s",buf);
-    return std::string(buf);
-}
-std::string Parsemap_Kd(const std::string& str) {
-    char buf[256];
-    sscanf(str.c_str()," map_Kd %80s",buf);
-    return std::string(buf);
-}
-std::string Parsemap_d(const std::string& str) {
-    char buf[256];
-    sscanf(str.c_str()," map_d %80s",buf);
-    return std::string(buf);
-}
-std::string Parsemap_bump(const std::string& str) {
-    char buf[256];
-    
-    sscanf(str.c_str()," map_bump %80s",buf);
-    return std::string(buf);
-}
-
-int ReflectCMTLMaterial(CMTLMaterial & mat) {
-
-    //printf();
-return 0;
-}
-
-std::unordered_map<std::string, std::unique_ptr<CMTLMaterial> > ParseMTL(std::string fname) {
+    float Parsed(const std::string &v_desc);
+     std::string ParseNEWMTL(const std::string &str);
+     float ParseNs(const std::string &v_desc);
+     float ParseNi(const std::string &v_desc);
+     int Parseillum(const std::string &v_desc);
+     CMTLColor ParseKa(const std::string &v_desc);
+     std::string Parsemap_bump(const std::string &str);
+     CMTLColor ParseKd(const std::string &v_desc);
+     std::string Parsemap_d(const std::string &str);
+     CMTLColor ParseKs(const std::string &v_desc);
+     CMTLColor ParseKe(const std::string &v_desc);
+     CMTLColor ParseTf(const std::string &v_desc);
+     std::string Parsemap_Ka(const std::string &str);
+     std::string Parsemap_Kd(const std::string &str);
+};
+MTLParser::MTLParser(const std::string& fname) {
     printf("Load MTL file %s \n",fname.c_str());
     std::ifstream tst;
-    std::unordered_map<std::string, std::unique_ptr<CMTLMaterial> > res;
 
     std::string mtln("default_mtl");
 
@@ -575,33 +511,33 @@ std::unordered_map<std::string, std::unique_ptr<CMTLMaterial> > ParseMTL(std::st
         for( std::string line; getline( tst, line ); ) {
             if (!line.find("newmtl") ){
                     mtln = ParseNEWMTL(line);
-                    if (res.count(mtln) == 0 ) {/* all materials are unique*/
-                        res[mtln] = std::unique_ptr<CMTLMaterial>(new CMTLMaterial());
+                    if (d_materials.count(mtln) == 0 ) {/* all materials are unique*/
+                        d_materials[mtln] = std::unique_ptr<CMTLMaterial>(new CMTLMaterial());
                     }
             } else if (line.find("map_Ka" ) != std::string::npos) {
-                    res[mtln]->map_Ka = Parsemap_Ka(line);  
+                    d_materials[mtln]->map_Ka = Parsemap_Ka(line);
             } else if (line.find("map_Kd") != std::string::npos) {
-                    res[mtln]->map_Kd = Parsemap_Kd(line);  
+                    d_materials[mtln]->map_Kd = Parsemap_Kd(line);
             } else if (line.find("map_bump")!= std::string::npos ) {
-                    res[mtln]->map_bump = Parsemap_bump(line);  
+                    d_materials[mtln]->map_bump = Parsemap_bump(line);
             } else if (line.find("map_d")!= std::string::npos ) {
-                    res[mtln]->map_d = Parsemap_d(line);  
+                    d_materials[mtln]->map_d = Parsemap_d(line);
             //} else if (line.find("Ns") != std::string::npos) {
-            //        res[mtln]->Ns = ParseNs(line);  
+            //        res[mtln]->Ns = ParseNs(line);
             //} else if (line.find("Ni") != std::string::npos) {
-            //        res[mtln]->Ni = ParseNi(line);  
+            //        res[mtln]->Ni = ParseNi(line);
             // } else if (line.find("d") != std::string::npos) {
             //         res[mtln]->Ni = Parsed(line);
             //} else if (line.find("Ka") != std::string::npos) {
-            //        res[mtln]->Ka = ParseKa(line);  
+            //        res[mtln]->Ka = ParseKa(line);
             //} else if (!line.find("Kd") != std::string::npos) {
             //        res[mtln]->Kd = ParseKd(line);
             //} else if (line.find("Ks") != std::string::npos) {
             //        res[mtln]->Ks = ParseKs(line);
             //} else if (line.find("Ke") != std::string::npos) {
-            //        res[mtln]->Ke = ParseKe(line);      
+            //        res[mtln]->Ke = ParseKe(line);
             //} else if (line.find("Tf") != std::string::npos) {
-            //        res[mtln]->Tf = ParseTf(line);  
+            //        res[mtln]->Tf = ParseTf(line);
             //} else if (line.find("illum") != std::string::npos) {
             //        res[mtln]->illum = Parseillum(line);
 
@@ -610,18 +546,94 @@ std::unordered_map<std::string, std::unique_ptr<CMTLMaterial> > ParseMTL(std::st
         }
     } else {
         printf ("Unable open MTL file %s\n", fname.c_str());
-    } 
+    }
 
     tst.close();
 
 
-    if (res.empty()) {
+    if (d_materials.empty()) {
         printf ("Add empty material\n");
-        res["default"] = (std::unique_ptr<CMTLMaterial>(new CMTLMaterial()));
+        d_materials["default"] = (std::unique_ptr<CMTLMaterial>(new CMTLMaterial()));
     }
-    return res;
+
 
 }
+std::string MTLParser::ParseNEWMTL(const std::string& str) {
+    char buf[256];
+    sscanf(str.c_str(),"newmtl %80s",buf);
+    return std::string(buf);
+}
+
+float MTLParser::ParseNs(const std::string& v_desc) {
+    float r = 0.0;
+    sscanf(v_desc.c_str()," Ns %f", &r);
+    return r;
+}
+float MTLParser::ParseNi(const std::string& v_desc) {
+    float r = 0.0;
+    sscanf(v_desc.c_str()," Ni %f", &r);
+    return r;
+}
+float MTLParser::Parsed(const std::string& v_desc) {
+    float r = 0.0;
+    sscanf(v_desc.c_str()," d %f", &r);
+    return r;
+}
+
+int MTLParser::Parseillum(const std::string& v_desc) {
+    int r = 0;
+    sscanf(v_desc.c_str()," illum %d", &r);
+    return r;
+}
+
+CMTLColor MTLParser::ParseKa(const std::string& v_desc) {
+    CMTLColor c;
+    sscanf(v_desc.c_str()," Ka %f %f %f", &c.r, &c.g, &c.b);
+    return c;
+}
+CMTLColor MTLParser::ParseKd(const std::string& v_desc) {
+    CMTLColor c;
+    sscanf(v_desc.c_str()," Kd %f %f %f", &c.r, &c.g, &c.b);
+    return c;
+}
+CMTLColor MTLParser::ParseKs(const std::string& v_desc) {
+    CMTLColor c;
+    sscanf(v_desc.c_str()," Ks %f %f %f", &c.r, &c.g, &c.b);
+    return c;
+}
+CMTLColor MTLParser::ParseKe(const std::string& v_desc) {
+    CMTLColor c;
+    sscanf(v_desc.c_str()," Ke %f %f %f", &c.r, &c.g, &c.b);
+    return c;
+}
+CMTLColor MTLParser::ParseTf(const std::string& v_desc) {
+    CMTLColor c;
+    sscanf(v_desc.c_str()," Tf %f %f %f", &c.r, &c.g, &c.b);
+    return c;
+}
+
+std::string MTLParser::Parsemap_Ka(const std::string& str) {
+    char buf[256];
+    sscanf(str.c_str()," map_Ka %80s",buf);
+    return std::string(buf);
+}
+std::string  MTLParser::Parsemap_Kd(const std::string& str) {
+    char buf[256];
+    sscanf(str.c_str()," map_Kd %80s",buf);
+    return std::string(buf);
+}
+std::string MTLParser::Parsemap_d(const std::string& str) {
+    char buf[256];
+    sscanf(str.c_str()," map_d %80s",buf);
+    return std::string(buf);
+}
+std::string MTLParser::Parsemap_bump(const std::string& str) {
+    char buf[256];
+    
+    sscanf(str.c_str()," map_bump %80s",buf);
+    return std::string(buf);
+}
+
 
 class SObjModel {
     public: 
@@ -654,7 +666,7 @@ class SObjModel {
         std::unordered_map<std::string, unsigned int > submesh_vbo;
         std::unordered_map<std::string, unsigned int > submesh_vao;
         std::unordered_map<std::string, std::unique_ptr<STexture> > d_textures;
-        std::unordered_map<std::string, std::unique_ptr<CMTLMaterial> > d_materials;
+        std::unordered_map<std::string, std::shared_ptr<CMTLMaterial> > d_materials;
 
         bool flag_normals = false;
         
@@ -722,11 +734,19 @@ SObjModel::SObjModel(const std::string&  fname)
     /* load descriptor*/
     unsigned int temp_vao;
     unsigned int temp_vbo;
-    if (parser->d_mtllibs.empty())
-        d_materials = ParseMTL("default.mtl");
-    else
-        d_materials = ParseMTL(parser->d_mtllibs[0]);
-    printf("materials file parsed\n");
+    {
+        if (parser->d_mtllibs.empty())
+        {
+            MTLParser mtl_p("default.mtl");
+            d_materials = mtl_p.d_materials; //OMG copy!! FIX ME
+        }
+        else
+        {
+             MTLParser mtl_p(parser->d_mtllibs[0]);
+            d_materials = mtl_p.d_materials; //OMG copy!! FIX ME
+        }
+    }
+
 
     texDiffuse.reset(new STexture("empty_texture.png"));
     if (!texDiffuse->IsReady) {
