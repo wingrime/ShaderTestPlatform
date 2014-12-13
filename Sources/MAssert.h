b42c24b4fc69b21750e170a2f79c33644e61c883
Alexsey Shestacov <wingrime@gmail.com>
2014-12-13 19:45:01 +0500

    Initial assert Macro definition - toggle breakpoint

--- Sources/MAssert.h
+++ Sources/MAssert.h
@@ -1,4 +1,10 @@
 #ifndef MASSERT_H
 #define MASSERT_H
+#pragma once
+#ifndef __BUILD_DROP_ASSERT__
+#define MASSERT(a) {if (a) {asm ("int $3");}}
+#else
+#define MASSERT(a)
+#endif
 
 #endif // MASSERT_H
