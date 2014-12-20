aeb735dd1d2fe109f593bb31c16278fbc35d081e
Alexsey Shestacov <wingrime@gmail.com>
2014-12-20 23:01:40 +0500

    Add D_TRAP macro for INT3 debuger call

--- Sources/MAssert.h
+++ Sources/MAssert.h
@@ -1,8 +1,9 @@
 #ifndef MASSERT_H
 #define MASSERT_H
 #pragma once
+#define D_TRAP() {asm ("int $3");}
 #ifndef __BUILD_DROP_ASSERT__
-#define MASSERT(a) {if (a) {asm ("int $3");}}
+#define MASSERT(a) {if (a) {D_TRAP();}}
 #else
 #define MASSERT(a)
 #endif
