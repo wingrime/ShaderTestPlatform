72c0655c0ba4226d1050c24b950166468095c8d9
Alexsey Shestacov <wingrime@gmail.com>
2015-01-02 17:57:50 +0500

    Add cubemap generator shaders (half workable)

---
+++ Build/shaders/cubemap_gen.f
@@ -0,0 +1,53 @@
+/* Base Lighting Shader */
+#version 430 core
+//layout(location = 0) out vec4 dstColor;
+out vec4 color;
+//layout(location = 1) out vec4 worldNormal; /*SSAO*/ 
+const float pi = 3.14159;
+
+
+in vec3 lightPos;
+in vec3 vPos;
+in vec2 uv;
+in vec3 t_normal;
+
+
+
+
+
+uniform sampler2D texIMG;
+uniform sampler2D texture_alpha_sampler;
+
+
+uniform float lightIntensity = 1.0;
+uniform float shadowPenumbra = 200.0;
+uniform float ambientIntensity = 0.1;
+/*lambertian cosine law with normalization factor*/
+float lambert(vec3 n , vec3 l)  {
+	return max(0.0f, dot(n,l));
+}
+void main() 
+{
+
+
+	float texture_alpha = texture(texture_alpha_sampler,uv).r;	
+
+	vec3 n = normalize(t_normal);
+	vec3 v  = normalize(-vPos);
+	vec3 l  = normalize( lightPos - vPos);
+	vec3 h = normalize(v+l);
+
+
+	vec3 texColor = (texture2D( texIMG,uv).rgb);
+	vec3 diffColor = texColor;//vec3(1.0f,0.71f,0.29f);
+	vec3 specColor =  vec3(1.0);//vec3(1.0f,0.71f,0.29f);
+
+
+	vec3 diff = lambert (n,l)*texColor;
+
+	//dstColor = vec4(vec3(float(gl_Layer)/6.0 ),1.0);
+	color = vec4(vec3(diff ),1.0);
+	//dstColor = vec4(vec3(n),1.0);
+	//dstColor = vec4(vec3(1.0),1.0);
+
+}
