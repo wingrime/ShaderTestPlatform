b0b7f7ac10b8fbbee9a0a7cf17b6ae15406d9fe0
Alexsey Shestacov <wingrime@gmail.com>
2015-02-15 23:53:51 +0500

    1)Use direct plane specification for ortigonal projection matrix frusturm planes 2)fix infiniry far projection matrix

--- Sources/r_projmat.h
+++ Sources/r_projmat.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "mat_math.h"
 #include <cmath>
+/*TODO add constructors based on frustrum planes*/
+
 class SPerspectiveProjectionMatrix: public SMat4x4 {
 public: 
     	SPerspectiveProjectionMatrix(float Near, float Far,float Aspect, float FOV) : SMat4x4() {
@@ -49,17 +51,17 @@ public:
 
 class SOrtoProjectionMatrix: public SMat4x4 {
 public: 
-        SOrtoProjectionMatrix(float Near, float Far) : SMat4x4() {
+        SOrtoProjectionMatrix(float Near, float Far,float r,float t, float l , float b) : SMat4x4() {
          
 
          /*Orto*/
 
          float f = Far;
          float n = Near;
-         float r = 1;
-         float t = 1;
-         float l = -1;
-         float b = -1;
+         //float r = 1;
+         //float t = 1;
+         //float l = -1;
+         //float b = -1;
          a11 = 2*n / (r - l );
          a13 = (r+l)/ (r-l );
          a22  = 2*n/ (t -b);
@@ -71,16 +73,17 @@ public:
 
     };
 };
-class SInfinityFarMatrix: public SMat4x4 {
+class SInfinityFarProjectionMatrix: public SMat4x4 {
 public:
-    SInfinityFarMatrix(float FOV) : SMat4x4() {
+    SInfinityFarProjectionMatrix(float Near,float Aspect,float FOV) : SMat4x4() {
 
         float focal_length = 1.0 / tan(  FOV / 2.0);
          a11 = focal_length;
-         a22  = (focal_length/FOV);
-         a33 = -1.0;
-         a34 = -2.0* (1.0f);
+         a22  = focal_length/Aspect;
+         a33 = -1;
+         a34 =  -2 * Near;
          a43 = -1.0f;
+         a44 = 0.0f;
 
     };
 };
