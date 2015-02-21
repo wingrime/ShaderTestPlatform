0d963312846597a56010490d95df685826d06533
Alexsey Shestacov <wingrime@gmail.com>
2015-02-21 23:44:07 +0500

    Fix string_format issue - passing string not by reference

--- Sources/string_format.cpp
+++ Sources/string_format.cpp
@@ -6,7 +6,7 @@
 
 #include "string_format.h"
 
-std::string string_format(const std::string fmt_str, ...) {
+std::string string_format(const std::string& fmt_str, ...) {
     int final_n, n = ((int)fmt_str.size()) * 2; /* reserve 2 times as much as the length of the fmt_str */
     std::unique_ptr<char[]> formatted;
     va_list ap;
