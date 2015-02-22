821e8c0a9be6d775dc933ec0584121e72a440ada
Alexsey Shestacov <wingrime@gmail.com>
2015-02-22 19:11:39 +0500

    Fix Ortographic projection matrix

--- Sources/r_projmat.h
+++ Sources/r_projmat.h
@@ -18,59 +18,16 @@ public:
          a43 = -1.0f;
          a44 = 0.0f;
 
-         /*Orto*/
-/*
-         float f = 1000;
-         float n = 1;
-         float r = 1;
-         float t = 1;
-         float l = -1;
-         float b = -1;
-         a11 = 2*n / (r - l );
-         a13 = (r+l)/ (r-l );
-         a22  = 2*n/ (t -b);
-         a23 = (t+b) / (t-b);
-         a33 = -((f+n)/ (f-n));
-         a34 =  -((2 *f*n) / ( f-n));
-         a43 = -1.0f;
-         a44 = 0.0f;
-*/
-         /*
-        //infinity far;
-         a11 = focal_length;
-         a22  = (focal_length/Aspect);
-         a33 = -1.0;
-         a34 = -2.0* (1.0f);
-         a43 = -1.0f;
-         a44 = 0.0f;
-         printf("proj\n");
-         Reflect();
-*/
     };
 };
 
 class SOrtoProjectionMatrix: public SMat4x4 {
 public: 
-        SOrtoProjectionMatrix(float Near, float Far,float r,float t, float l , float b) : SMat4x4() {
-         
-
-         /*Orto*/
-
-         float f = Far;
-         float n = Near;
-         //float r = 1;
-         //float t = 1;
-         //float l = -1;
-         //float b = -1;
-         a11 = 2*n / (r - l );
-         a13 = (r+l)/ (r-l );
-         a22  = 2*n/ (t -b);
-         a23 = (t+b) / (t-b);
-         a33 = -((f+n)/ (f-n));
-         a34 =  -((2 *f*n) / ( f-n));
-         a43 = -1.0f;
-         a44 = 0.0f;
-
+        SOrtoProjectionMatrix(float n, float f,float r,float t, float l , float b) : SMat4x4() {
+         a11 = 2.0 / (r-l); a12 = 0.0;         a13 = 0.0;       a14 = -(r+l)/(r-l);
+         a21 = 0.0;         a22 = 2.0/(t-b);   a23 = 0.0;       a24 = -(t+b)/(t-b);
+         a31 = 0.0;         a32 = 0.0;         a33 = -2.0/(f-n);a34 = -(f+n)/(f-n);
+         a41 = 0.0;         a42 = 0.0;         a43 = 0.0;       a44 = 1.0;
     };
 };
 class SInfinityFarProjectionMatrix: public SMat4x4 {
