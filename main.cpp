9ebfcad6b8068750913b0bc8100f4d0472e05d13
Alexsey Shestacov <wingrime@gmail.com>
2015-05-10 22:31:08 +0500

    Initial imGUI integration

--- main.cpp
+++ main.cpp
@@ -26,6 +26,217 @@ Global TODOs:
 
 #include "oglGlutInit.h"
 
+#include <imgui.h>
+/*imGUI data*/
+static double       g_Time = 0.0f;
+static bool         g_MousePressed[3] = { false, false, false };
+static float        g_MouseWheel = 0.0f;
+static GLuint       g_FontTexture = 0;
+static int          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
+static int          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
+static int          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
+static size_t       g_VboSize = 0;
+static unsigned int g_VboHandle = 0, g_VaoHandle = 0;
+static void ImGui_Impl_RenderDrawLists(ImDrawList** const cmd_lists, int cmd_lists_count)
+{
+    if (cmd_lists_count == 0)
+        return;
+
+    // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled
+    GLint last_program, last_texture;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+    glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_SCISSOR_TEST);
+    glActiveTexture(GL_TEXTURE0);
+
+    // Setup orthographic projection matrix
+    const float width = ImGui::GetIO().DisplaySize.x;
+    const float height = ImGui::GetIO().DisplaySize.y;
+    const float ortho_projection[4][4] =
+    {
+        { 2.0f/width,	0.0f,			0.0f,		0.0f },
+        { 0.0f,			2.0f/-height,	0.0f,		0.0f },
+        { 0.0f,			0.0f,			-1.0f,		0.0f },
+        { -1.0f,		1.0f,			0.0f,		1.0f },
+    };
+    glUseProgram(g_ShaderHandle);
+    glUniform1i(g_AttribLocationTex, 0);
+    glUniformMatrix4fv(g_AttribLocationProjMtx, 1, GL_FALSE, &ortho_projection[0][0]);
+
+    // Grow our buffer according to what we need
+    size_t total_vtx_count = 0;
+    for (int n = 0; n < cmd_lists_count; n++)
+        total_vtx_count += cmd_lists[n]->vtx_buffer.size();
+    glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
+    size_t needed_vtx_size = total_vtx_count * sizeof(ImDrawVert);
+    if (g_VboSize < needed_vtx_size)
+    {
+        g_VboSize = needed_vtx_size + 5000 * sizeof(ImDrawVert);  // Grow buffer
+        glBufferData(GL_ARRAY_BUFFER, g_VboSize, NULL, GL_STREAM_DRAW);
+    }
+
+    // Copy and convert all vertices into a single contiguous buffer
+    unsigned char* buffer_data = (unsigned char*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+    if (!buffer_data)
+        return;
+    for (int n = 0; n < cmd_lists_count; n++)
+    {
+        const ImDrawList* cmd_list = cmd_lists[n];
+        memcpy(buffer_data, &cmd_list->vtx_buffer[0], cmd_list->vtx_buffer.size() * sizeof(ImDrawVert));
+        buffer_data += cmd_list->vtx_buffer.size() * sizeof(ImDrawVert);
+    }
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(g_VaoHandle);
+
+    int cmd_offset = 0;
+    for (int n = 0; n < cmd_lists_count; n++)
+    {
+        const ImDrawList* cmd_list = cmd_lists[n];
+        int vtx_offset = cmd_offset;
+        const ImDrawCmd* pcmd_end = cmd_list->commands.end();
+        for (const ImDrawCmd* pcmd = cmd_list->commands.begin(); pcmd != pcmd_end; pcmd++)
+        {
+            if (pcmd->user_callback)
+            {
+                pcmd->user_callback(cmd_list, pcmd);
+            }
+            else
+            {
+                glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)pcmd->texture_id);
+                glScissor((int)pcmd->clip_rect.x, (int)(height - pcmd->clip_rect.w), (int)(pcmd->clip_rect.z - pcmd->clip_rect.x), (int)(pcmd->clip_rect.w - pcmd->clip_rect.y));
+                glDrawArrays(GL_TRIANGLES, vtx_offset, pcmd->vtx_count);
+            }
+            vtx_offset += pcmd->vtx_count;
+        }
+        cmd_offset = vtx_offset;
+    }
+
+    // Restore modified state
+    glBindVertexArray(0);
+    glUseProgram(last_program);
+    glDisable(GL_SCISSOR_TEST);
+    glBindTexture(GL_TEXTURE_2D, last_texture);
+}
+
+void ImGui_CreateFontsTexture()
+{
+    ImGuiIO& io = ImGui::GetIO();
+
+    unsigned char* pixels;
+    int width, height;
+    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);   // Load as RGBA 32-bits for OpenGL3 demo because it is more likely to be compatible with user's existing shader.
+
+    glGenTextures(1, &g_FontTexture);
+    glBindTexture(GL_TEXTURE_2D, g_FontTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+    // Store our identifier
+    io.Fonts->TexID = (void *)(intptr_t)g_FontTexture;
+}
+
+bool ImGui_Impl_CreateDeviceObjects()
+{
+    const GLchar *vertex_shader =
+        "#version 330\n"
+        "uniform mat4 ProjMtx;\n"
+        "in vec2 Position;\n"
+        "in vec2 UV;\n"
+        "in vec4 Color;\n"
+        "out vec2 Frag_UV;\n"
+        "out vec4 Frag_Color;\n"
+        "void main()\n"
+        "{\n"
+        "	Frag_UV = UV;\n"
+        "	Frag_Color = Color;\n"
+        "	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n"
+        "}\n";
+
+    const GLchar* fragment_shader =
+        "#version 330\n"
+        "uniform sampler2D Texture;\n"
+        "in vec2 Frag_UV;\n"
+        "in vec4 Frag_Color;\n"
+        "out vec4 Out_Color;\n"
+        "void main()\n"
+        "{\n"
+        "	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
+        "}\n";
+
+    g_ShaderHandle = glCreateProgram();
+    g_VertHandle = glCreateShader(GL_VERTEX_SHADER);
+    g_FragHandle = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(g_VertHandle, 1, &vertex_shader, 0);
+    glShaderSource(g_FragHandle, 1, &fragment_shader, 0);
+    glCompileShader(g_VertHandle);
+    glCompileShader(g_FragHandle);
+    glAttachShader(g_ShaderHandle, g_VertHandle);
+    glAttachShader(g_ShaderHandle, g_FragHandle);
+    glLinkProgram(g_ShaderHandle);
+
+    g_AttribLocationTex = glGetUniformLocation(g_ShaderHandle, "Texture");
+    g_AttribLocationProjMtx = glGetUniformLocation(g_ShaderHandle, "ProjMtx");
+    g_AttribLocationPosition = glGetAttribLocation(g_ShaderHandle, "Position");
+    g_AttribLocationUV = glGetAttribLocation(g_ShaderHandle, "UV");
+    g_AttribLocationColor = glGetAttribLocation(g_ShaderHandle, "Color");
+
+    glGenBuffers(1, &g_VboHandle);
+
+    glGenVertexArrays(1, &g_VaoHandle);
+    glBindVertexArray(g_VaoHandle);
+    glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
+    glEnableVertexAttribArray(g_AttribLocationPosition);
+    glEnableVertexAttribArray(g_AttribLocationUV);
+    glEnableVertexAttribArray(g_AttribLocationColor);
+
+#define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
+    glVertexAttribPointer(g_AttribLocationPosition, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, pos));
+    glVertexAttribPointer(g_AttribLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, uv));
+    glVertexAttribPointer(g_AttribLocationColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, col));
+#undef OFFSETOF
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    ImGui_CreateFontsTexture();
+
+    return true;
+}
+int imGuiSetup() {
+    // Set up ImGui
+        ImGuiIO& guiIO = ImGui::GetIO();
+        guiIO.DisplaySize.x = 1000.0f;
+        guiIO.DisplaySize.y =  1000.0f;
+        guiIO.DeltaTime = 1.0f / 60.0f;
+        //guiIO.IniFilename = "imgui.ini";
+        guiIO.RenderDrawListsFn = ImGui_Impl_RenderDrawLists;
+        guiIO.KeyMap[0] = 9;    // tab
+        guiIO.KeyMap[1] = GLUT_KEY_LEFT;    // Left
+        guiIO.KeyMap[2] = GLUT_KEY_RIGHT;   // Right
+        guiIO.KeyMap[3] = GLUT_KEY_UP;      // Up
+        guiIO.KeyMap[4] = GLUT_KEY_DOWN;    // Down
+        guiIO.KeyMap[5] = GLUT_KEY_HOME;    // Home
+        guiIO.KeyMap[6] = GLUT_KEY_END;     // End
+        guiIO.KeyMap[7] = 127;  // Delete
+        guiIO.KeyMap[8] = 8;    // Backspace
+        guiIO.KeyMap[9] = 13;   // Enter
+        guiIO.KeyMap[10] = 27;  // Escape
+        guiIO.KeyMap[11] = 1;   // ctrl-A
+        guiIO.KeyMap[12] = 3;   // ctrl-C
+        guiIO.KeyMap[13] = 22;  // ctrl-V
+        guiIO.KeyMap[14] = 24;  // ctrl-X
+        guiIO.KeyMap[15] = 25;  // ctrl-Y
+        guiIO.KeyMap[16] = 26;  // ctrl-Z
+
+
+    return 0;
+}
 /*Singletons */
 /*
  * Must be moved to Singltone interface as be ready
@@ -40,7 +251,8 @@ void display ()
     /* main engine loop */
    static std::chrono::steady_clock::time_point last_t;
    //full time difference;
-   float dt = std::chrono::duration<float, std::milli> ( (std::chrono::steady_clock::now() - last_t) ).count();
+   float dt = std::chrono::duration<float, std::milli> ( (std::chrono::steady_clock::now() - last_t) ).count();\
+   float dt_seconds = std::chrono::duration<float, std::ratio<1>> ( (std::chrono::steady_clock::now() - last_t) ).count();
    // update time
    last_t = std::chrono::steady_clock::now();
 
@@ -48,7 +260,25 @@ void display ()
 
     SScene * sc = MainScene::GetInstance();
     auto start = std::chrono::steady_clock::now();
+    if (!g_FontTexture)
+        ImGui_Impl_CreateDeviceObjects();
     sc->Render();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.DeltaTime = dt_seconds;
+    ImGui::NewFrame();
+    //ImGui::Begin("My window");
+    ImGui::Text("Hello, world!");
+    ImGui::Text("Hello, world!");
+    ImGui::Text("Hello, world!");
+     static float f = 0.0f;
+     ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+     ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+     ///ImGui::End();
+
+    ImGui::Render();
+
+
     if (g_scriptCallFrequency) {
         if ((g_frameNumber % g_scriptCallFrequency) == 0)
         {
@@ -66,8 +296,13 @@ void reshape ( int w, int h )
     SScene * sc = MainScene::GetInstance();
 
     sc->Reshape(w,h);
+    /*ImGui hook*/
+    ImGuiIO& io = ImGui::GetIO();
+    io.DisplaySize.x = (float)w;
+    io.DisplaySize.y =  (float)h;
 
 }
+
 void key ( unsigned char key, int x, int y )
 {
     SScene * sc = MainScene::GetInstance();
@@ -120,8 +355,21 @@ else
 
 }
 
+void mouse_move_passive (  int x , int y) {
+
+    ImGuiIO& io = ImGui::GetIO();
+    /*imgui hook*/
+    io.MousePos = ImVec2((float)x, (float)y);
+
+}
 void mouse_move (  int x , int y) {
     SScene * sc = MainScene::GetInstance();
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    /*imgui hook*/
+    io.MousePos = ImVec2((float)x, (float)y);
+
 
     static int x_base = -10000 ;
     static int y_base = -10000 ;
@@ -158,7 +406,15 @@ sc->dbg_ui.UpdateCfgLabel();
 }
 
 void mouse(int button, int state, int x, int y)  {
-  
+    ImGuiIO& io = ImGui::GetIO();
+    /*imgui hook*/
+    io.MousePos = ImVec2((float)x, (float)y);
+    if (button == GLUT_LEFT_BUTTON)
+     io.MouseDown[0] = (state == GLUT_DOWN); // don't know why it's inversed but thats works
+    else if (button == GLUT_MIDDLE_BUTTON)
+     io.MouseDown[1] = (state == GLUT_DOWN);
+    else if (button == GLUT_RIGHT_BUTTON)
+     io.MouseDown[2] = (state == GLUT_DOWN);
 }
 int ToggleFullscreen() {
     static bool d_toggle_fullscreen  = false;
@@ -234,7 +490,10 @@ int initHooks() {
     glutMotionFunc(mouse_move);
     glutKeyboardFunc(key);
     glutSpecialFunc(special);
+    glutPassiveMotionFunc(mouse_move_passive);
 }
+
+
 
 int main ( int argc, char * argv [] )
 {
@@ -280,6 +539,8 @@ int main ( int argc, char * argv [] )
     LOGV("Create Scene");
     RBO  v(w,h);
     MainScene msc(&v);
+
+    imGuiSetup();
 
     //sc.reset(new SScene(&v));
     SScene * sc = MainScene::GetInstance();
