033b588f449236a6f3ec1c7e3137b3dc46491d67
wingrime <wingrime@gmail.com>
2015-12-17 20:54:17 +0500

    Always remove last executables before new build

--- BuildWin.bat
+++ BuildWin.bat
@@ -1,3 +1,5 @@
 del CMakeCache.txt
+del Build\main.exe
+del Build\tests.exe
 cmake -G "MinGW Makefiles" 
 mingw32-make -j8
--- BuildWinRelease.bat
+++ BuildWinRelease.bat
@@ -1,3 +1,5 @@
 del CMakeCache.txt
+del Build\main.exe
+del Build\tests.exe
 cmake -G "MinGW Makefiles" -DDEBUG_BUILD=OFF
 mingw32-make -j8
